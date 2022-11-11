5f9cbcbaa40bfe273969868101b1ea350cf6c182
Rajan Maharjan <61002477+Rajan66@users.noreply.github.com>
2022-11-11 15:08:51 +0545

    Add files via upload

---
+++ src/Qno1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int length, breadth;
+        double area;
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the length of the rectangle: ");
+        length = sc.nextInt();
+        System.out.print("Enter the breadth of the rectangle: ");
+        breadth = sc.nextInt();
+
+        area = length * breadth;
+        System.out.println("Area of the rectangle" + area);
+        sc.close();
+    }
+
+}
